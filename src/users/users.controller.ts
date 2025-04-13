7c9234b4aafdfda05ff0fe31757b7cc31d32cfda
Amit Kumar <knightbuster18@gmail.com>
2025-04-13 22:50:56 +0530

    user services build with validation & error handling

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,32 +6,42 @@ import {
   Param,
   Patch,
   Post,
+  ParseIntPipe,
+  ValidationPipe,
 } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create.user.dto';
+import { UpdateUserDto } from './dto/update.user.dto';
 
 @Controller('users') // domain/users/
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
   @Get() // GET /users or /users?role=value&age=value
   findAll() {
-    return [];
+    return this.usersService.findAll();
   }
 
   @Get(':id') // GET /users/:id
-  findOne(@Param('id') id: string) {
-    return { id };
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.findOne(id);
   }
 
   @Post() // POST /users
-  create(@Body() user: {}) {
-    return user;
+  create(@Body(ValidationPipe) createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
   }
 
   @Patch(':id') // PATCH /users/:id
-  update(@Param('id') id: string, @Body() userUpdate: {}) {
-    return { id, ...userUpdate };
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body(ValidationPipe) updateUserDto: UpdateUserDto,
+  ) {
+    return this.usersService.update(id, updateUserDto);
   }
 
   @Delete(':id') // DELETE /users/:id
-  delete(@Param('id') id: string) {
-    return { id };
+  delete(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.delete(id);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,62 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create.user.dto';
+import { UpdateUserDto } from './dto/update.user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users = [
+    {
+      id: 1,
+      name: 'Leanne Graham',
+      email: 'grahamleanne@mail.com',
+    },
+    {
+      id: 2,
+      name: 'Ervin Howell',
+      email: 'howellervin@mail.com',
+    },
+    {
+      id: 3,
+      name: 'Clementine Brown',
+      email: 'brownclementine@mail.com',
+    },
+  ];
+
+  findAll() {
+    return this.users;
+  }
+
+  findOne(id: number) {
+    const user = this.users.find((user) => user.id === id);
+
+    if (!user) throw new NotFoundException('User not found');
+    return user;
+  }
+
+  create(createUserDto: CreateUserDto) {
+    const usersByHighestId = [...this.users].sort((a, b) => b.id - a.id);
+    const newUser = {
+      id: usersByHighestId[0].id + 1,
+      ...createUserDto,
+    };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    this.users = this.users.map((user) => {
+      if (user.id === id) {
+        return { ...user, ...updateUserDto };
+      }
+      return user;
+    });
+    return this.findOne(id);
+  }
+
+  delete(id: number) {
+    const removedUser = this.findOne(id);
+
+    this.users = this.users.filter((user) => user.id !== id);
+    return removedUser;
+  }
+}
