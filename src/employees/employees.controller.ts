45459d3f5fdb3671e2541e84a5e3333e96a94ecc
Amit Kumar <knightbuster18@gmail.com>
2025-04-14 19:20:45 +0530

    database & orm setup

---
+++ src/employees/employees.controller.ts
@@ -0,0 +1,45 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { EmployeesService } from './employees.service';
+import { Prisma } from 'generated/prisma';
+
+@Controller('employees')
+export class EmployeesController {
+  constructor(private readonly employeesService: EmployeesService) {}
+
+  @Post()
+  create(@Body() createEmployeeDto: Prisma.EmployeeCreateInput) {
+    return this.employeesService.create(createEmployeeDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.employeesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.employeesService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateEmployeeDto: Prisma.EmployeeUpdateInput,
+  ) {
+    return this.employeesService.update(id, updateEmployeeDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.employeesService.remove(id);
+  }
+}
